59f38f831bf8f951f2507c178adcc728f2811024
James Weber <jpweber@gmail.com>
2017-05-04 20:05:16 -0400

    Initial Commit

---
+++ kContext/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  kContext
+//
+//  Created by James Weber on 5/4/17.
+//  Copyright © 2017 James Weber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
