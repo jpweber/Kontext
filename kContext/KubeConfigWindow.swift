62fbbcff0ba7fd1ed9dd8f1dfefd813995c78868
Jim Weber <jim.weber@crosschx.com>
2017-10-05 13:27:20 -0400

    Changed  text area to nxtextview from nxtextfieldcell

--- kContext/KubeConfigWindow.swift
+++ kContext/KubeConfigWindow.swift
@@ -11,7 +11,8 @@ import Cocoa
 class KubeConfigWindow: NSWindowController {
 
     
-    @IBOutlet weak var configLabel: NSTextFieldCell!
+    @IBOutlet var configText: NSTextView!
+    
     
     convenience init() {
         self.init(windowNibName: "KubeConfigWindow")
@@ -24,7 +25,7 @@ class KubeConfigWindow: NSWindowController {
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         let menuController = ContextMenuController()
         let rc = menuController.shell(launchPath: "/usr/local/bin/kubectl", arguments: ["config", "view"])
-        configLabel.title = rc.0!
+        configText.textStorage?.append(NSAttributedString(string: rc.0!, attributes: nil))
     }
     
 }
